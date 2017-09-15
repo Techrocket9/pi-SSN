ca6785aa0a3fbbecad4a83fd7e803b15c701ebe5
Techrocket9 <Techrocket9>
2017-09-14 19:30:17 -0700

    Reading from file.

--- πSSN/Program.cs
+++ πSSN/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,16 @@ namespace πSSN
     {
         static void Main(string[] args)
         {
+            var srcFilePath = @"C:\Users\Techr\Downloads\y-cruncher v0.7.3.9474\Binaries\Pi - Dec - Chudnovsky.txt";
+            using (var fs = File.Open(srcFilePath, FileMode.Open, FileAccess.Read, FileShare.Read))
+            using (var stream = new StreamReader(fs))
+            {
+                char firstChar = (char) stream.Read();
+                char secondChar = (char)stream.Read();
+                char thirdChar = (char)stream.Read();
+
+                Console.WriteLine($"{firstChar}, {secondChar}, {thirdChar}");
+            }
         }
     }
 }
