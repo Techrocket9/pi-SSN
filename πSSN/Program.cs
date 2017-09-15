e1d3a227b764550e060795bac2fda88c2d84de44
Techrocket9 <Techrocket9>
2017-09-14 20:19:28 -0700

    Out of memory exception,

--- πSSN/Program.cs
+++ πSSN/Program.cs
@@ -15,12 +15,80 @@ namespace πSSN
             using (var fs = File.Open(srcFilePath, FileMode.Open, FileAccess.Read, FileShare.Read))
             using (var stream = new StreamReader(fs))
             {
-                char firstChar = (char) stream.Read();
-                char secondChar = (char)stream.Read();
-                char thirdChar = (char)stream.Read();
+                int foundSSNs = 0;
+                uint digitOffset = 1;
+                var ssnOffsets = new uint[1_000_000_000];
+                var ringBuffer = new StringBuilder(9);
 
-                Console.WriteLine($"{firstChar}, {secondChar}, {thirdChar}");
+                //Discard 3 ( the . will be dropped in the first pass through the main loop)
+                stream.Read();
+
+                // Load ringbuffer with starter data
+                for (int i = 0; i < 9; ++i)
+                {
+                    ringBuffer.Append((char)stream.Read());
+                }
+
+                // Actually do the work
+                for(;;)
+                {
+                    var nextVal = stream.Read();
+                    if (-1 == nextVal)
+                    {
+                        Console.WriteLine($"Exhaused available digits of π. Found {foundSSNs} social security numbers.");
+                        break;
+                    }
+                    ++digitOffset;
+                    ringBuffer.Remove(0, 1); // Pop off last character
+                    ringBuffer.Append((char)nextVal); // Push on next character
+                    var ssn = int.Parse(ringBuffer.ToString());
+
+                    if (0 == ssnOffsets[ssn])
+                    {
+                        ssnOffsets[ssn] = digitOffset;
+                        ++foundSSNs;
+                        if (0 == foundSSNs % 10_000)
+                        {
+                            Console.WriteLine($"Found {AddOrdinal(foundSSNs)} social security number in π at offset {digitOffset}");
+                        }
+                    }
+
+
+
+                    if (foundSSNs >= 1_000_000_000)
+                    {
+                        Console.WriteLine($"Found all SSNs in π");
+                        break;
+                    }
+                }
             }
+        }
+
+        // Taken from https://stackoverflow.com/a/20175/
+        public static string AddOrdinal(int num)
+        {
+            if (num <= 0) return num.ToString();
+
+            switch (num % 100)
+            {
+                case 11:
+                case 12:
+                case 13:
+                    return num + "th";
+            }
+
+            switch (num % 10)
+            {
+                case 1:
+                    return num + "st";
+                case 2:
+                    return num + "nd";
+                case 3:
+                    return num + "rd";
+                default:
+                    return num + "th";
+            }
+
         }
     }
 }
